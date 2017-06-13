34f3c0d68780117289312cf84c67b393cc94979b
ramror3 <rameshror3@gmail.com>
2017-06-13 15:37:41 +0530

    partials

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
 	validates :title, presence: true, length: { minimum: 3, maximum: 50 }
-	validates :description, presence:true, length: { minimum: 50, maximum: 300}
+	validates :description, presence:true, length: { minimum: 30, maximum: 300}
 end
