4468e6c246050879d2ed0c8c2e5900bd291e53e6
ramror3 <rameshror3@gmail.com>
2017-06-14 12:22:53 +0530

    signup users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   resources :articles
+  resources :users, except: [:new]
   root 'pages#home'
 
   get 'about', to: 'pages#about'
+  get 'signup', to: 'users#new'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
